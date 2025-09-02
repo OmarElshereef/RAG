28f5df87c28ede64decd8e27c08cefe3982a5f88
Omar Elshereef <omarelshereef@gmail.com>
2025-09-02 14:30:50 +0300

    feat: expand VectorDBEnums with PgVector-related distance methods and table schema

--- src/stores/vectorDB/VectorDBEnums.py
+++ src/stores/vectorDB/VectorDBEnums.py
@@ -3,8 +3,23 @@ from enum import Enum
 
 class VectorDBEnums(Enum):
     QDRANT = "QDRANT"
+    PGVECTOR = "PGVECTOR"
 
 
 class DistanceMethodEnums(Enum):
     COSINE = "cosine"
     DOT = "dot"
+
+
+class PgVectorDistanceMethodEnums(Enum):
+    COSINE = "vector_cosine_ops"
+    DOT = "vector_12_ops"
+
+
+class PgVecotrTableSchemeEnums(Enum):
+    ID = "id"
+    TEXT = "text"
+    METADATA = "metadata"
+    VECTOR = "vector"
+    CHUNK_ID = "chunk_id"
+    _PREFIX = "pgvector"
